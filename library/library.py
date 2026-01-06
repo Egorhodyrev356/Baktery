815afa1b217c21731fdf964634ff4fda9779b4e2
Egork_Hody <egorhodyrev37@gmail.com>
2026-01-06 20:09:38 +0300

    Добавлена работа с книгами через SQLAlchemy

---
+++ library/library.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///books.db')
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+book1 = Book(title="Гарри Поттер", author="Дж.К Роулинг")  # Добавляем первую книгу: "Гарри Поттер"
+book2 = Book(title="Властелин колец", author="Джон Толкин")
+book3 = Book(title='1984', author='Джордж Оруэлл')
+
+session.add(book1)
+session.add(book2)
+session.add(book3)
+session.commit()
+
+for book in session.query(Book).all():
+    print(f"{book.title} – {book.author}")
